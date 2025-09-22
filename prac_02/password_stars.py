5bd450999f45c3edda527503ead9f136c453398a
HuangZhaozhang <zhaozhang.huang@my.jcu.edu.au>
2025-09-22 18:30:52 +0800

    this is password_stars

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,31 @@
+"""
 MIN_LENGTH = 8
-password = input("Enter your password: ")
-while len(password) < MIN_LENGTH:
-    print("password is too short, please enter a longer password")
+function main():
+    get password
+    print_stars {password}
+function get_password():
+    get password
+    while len(password) < MIN_LENGTH:
+        print "password is too short, please enter a longer password"
+        get password
+    return password
+function print_stars(password, symbol = "*"):
+    print "Pythonista + symbol * len(password)"
+if __name__ == '__main__':
+    main()
+
+"""
+MIN_LENGTH = 8
+def main():
+    password = get_password()
+    print_stars(password)
+def get_password():
     password = input("Enter your password: ")
-print("Pythonista" + "*" * len(password))
+    while len(password) < MIN_LENGTH:
+        print("password is too short, please enter a longer password")
+        password = input("Enter your password: ")
+    return password
+def print_stars(password, symbol = "*"):
+    print("Pythonista: " + symbol * len(password))
+if __name__ == '__main__':
+    main()
