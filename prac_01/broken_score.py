1834112c12375627ce52451ef41d338cde06a384
HuangZhaozhang <zhaozhang.huang@my.jcu.edu.au>
2025-09-22 17:04:38 +0800

    this is broken_score.py

---
+++ prac_01/broken_score.py
@@ -0,0 +1,20 @@
+"""
+get score
+if score <= 0 or score > 100:
+    print "Invalid score"
+else if score>=90:
+    print "Excellent"
+else if score>=50:
+    print "Pass"
+else:
+    print "Fail"
+"""
+score = float(input("Score: "))
+if score<=0 or score>100:
+    print("Invalid score")
+elif score>=90:
+    print("Excellent")
+elif score>=50:
+    print("Pass")
+else:
+    print("Fail")
