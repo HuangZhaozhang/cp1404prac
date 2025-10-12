d909e3bfb44bb749a98b4b7eb1e657270ea49c71
HuangZhaozhang <zhaozhang.huang@my.jcu.edu.au>
2025-10-12 20:49:33 +0800

    this is list_warmup.py

---
+++ prac_04/list_warmup.py
@@ -0,0 +1,9 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+# 1. Turn first element to string "ten"
+numbers[0] = "ten"
+# 2. Turn finally element to 1
+numbers[-1] = 1
+# 3. Print all elements except the first two
+print(numbers[2:])
+# 4. Whether element 9 in print
+print(9 in numbers)
