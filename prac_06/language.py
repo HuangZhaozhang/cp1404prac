76cda7af118da2ef25517b1c8f9be534fb2e196a
HuangZhaozhang <zhaozhang.huang@my.jcu.edu.au>
2025-11-02 02:49:00 +0800

    practical 6 hw

---
+++ prac_06/language.py
@@ -0,0 +1,17 @@
+from prac_06.programming_language import ProgrammingLanguage
+def main():
+    #Create programming language objects
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    #Print the python object to verify the __str__ method
+    print(python)
+    #Create a list containing three objects
+    languages = [python, ruby, visual_basic]
+    #Loop to print dynamically typed languages
+    print("The dynamically typed languages are: ")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+if __name__ == "__main__":
+    main()
